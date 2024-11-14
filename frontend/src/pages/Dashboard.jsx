cd8c3ec5b63692504483706f00d943dcb04b211a
Piyush Dixit <70336252+Er-luffy-D@users.noreply.github.com>
2024-11-15 01:28:03 +0530

    added routes and fixed some issues

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="flex flex-col items-center justify-center w-full h-full p-4 bg-gray-100 rounded-lg shadow-lg">
+      <div className="text-xl font-semibold mb-4">Dashboard</div>
+      <button><Link to="/">Back</Link></button>
+    </div>
+  );
 }
 
 export default Dashboard
