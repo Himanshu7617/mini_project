8556d64e6490e0a6fdefb2036d10c14db47fbbf6
Himanshu <gghimanshu333@gmail.com>
2024-11-22 13:40:05 +0530

    clean up

--- backend/routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { translate,random } from '../controller/userController.js';
-
-const router=express.Router();
-
-router.route("/translate").post(translate);
-router.route("/random").get(random);
-
-export default router;
