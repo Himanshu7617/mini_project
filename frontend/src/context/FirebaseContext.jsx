6bc75d085f6dc5211cd2a89b1cb609fe10c308d8
Himanshu <gghimanshu333@gmail.com>
2024-12-31 22:57:58 +0530

    v2.0.0 working on extension cross communication

--- frontend/src/context/FirebaseContext.jsx
+++ frontend/src/context/FirebaseContext.jsx
@@ -27,13 +27,13 @@ const auth = getAuth(app);
 const FirebaseContext = (props) => {
 
     //user's info initialization
-    const [avatarIndex , setAvatarIndex] =  useState(0);
+    const [avatarIndex, setAvatarIndex] = useState(0);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null);
-    const [allWords , setAllWords] = useState([]);
+    const [allWords, setAllWords] = useState([]);
     const [userID, setUserID] = useState('');
     const [gotData, setGotData] = useState(false);
     const newWords = [];
@@ -93,6 +93,16 @@ const FirebaseContext = (props) => {
         }
     }
 
+    //storing the current user id to the indexedDB
+    const storeUserUID = async () => {
+
+        const valueToSend = "Hello from your website!";
+
+        // Send the value to the extension via `postMessage`
+        window.postMessage({ type: "FROM_WEBSITE", value: valueToSend }, "*");
+
+    }
+
     //checking where the user has logged in b4 or not
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -100,11 +110,11 @@ const FirebaseContext = (props) => {
                 setUser(user);
                 setUserID(user.uid);
                 setIsLoggedIn(true);
-                
-                if(!gotData ){
+
+                if (!gotData) {
                     getAllWords();
                     setGotData(true);
-                    
+
                 }
             } else {
                 setUser(null);
@@ -112,37 +122,33 @@ const FirebaseContext = (props) => {
         })
     }, [])
 
+
     useEffect(() => {
- 
-        const alreadyStoredID = localStorage.getItem("wordwise_user_id");
-    
-        if (!alreadyStoredID) {
-            localStorage.setItem("wordwise_user_id", userID);
-        }
-    }, [userID]);
-    
+        storeUserUID();
+    }, []);
+
 
     // useEffect(()=>{
     //   if(!gotData && user){
     //     getAllWords();
     //     setGotData(true);
-      
+
     //   }
     // },[user])
-    
-    
+
+
 
     //logging out the user
     const logOut = () => {
-        if ( user ) {
-            signOut(auth).then(() => { 
+        if (user) {
+            signOut(auth).then(() => {
 
                 console.log('logged out')
-                newWords.splice(0,newWords.length);
+                newWords.splice(0, newWords.length);
                 setAllWords([])
                 navigate('/');
                 setIsLoggedIn(false);
-                
+
             })
         }
     }
@@ -150,20 +156,20 @@ const FirebaseContext = (props) => {
     //getting all the words from the firestore
 
     const getAllWords = async () => {
-        newWords.splice(0,newWords.length);
+        newWords.splice(0, newWords.length);
         const user = auth.currentUser;
-        if(user !== null){
+        if (user !== null) {
             //create a users ref
             const currentUserRef = collection(wordsDB, 'users');
 
 
             //search the document where email === currentUser.email
-            const currentUserDocQuery = query(currentUserRef, where("email", "==",`${user.email}`))
+            const currentUserDocQuery = query(currentUserRef, where("email", "==", `${user.email}`))
             const currentUserDocRef = await getDocs(currentUserDocQuery);
             let currentUserDocID = '';
-            currentUserDocRef && currentUserDocRef.forEach((item)=>{
+            currentUserDocRef && currentUserDocRef.forEach((item) => {
                 const itemPath = item._key.path.segments;
-                currentUserDocID = itemPath[itemPath.length-1];
+                currentUserDocID = itemPath[itemPath.length - 1];
 
             })
             //create a words collection ref associated with the currentUser
@@ -171,58 +177,58 @@ const FirebaseContext = (props) => {
 
             //get the data
             const allWordsSnapshot = await getDocs(userWordsCollectionRef);
-            allWordsSnapshot.forEach((item)=> {
-                
+            allWordsSnapshot.forEach((item) => {
+
                 const newWord = {
-                    word : item.data().word,
-                    translation : item.data().translation,
-                    definition : item.data().definition, 
-                    example : item.data().example,
+                    word: item.data().word,
+                    translation: item.data().translation,
+                    definition: item.data().definition,
+                    example: item.data().example,
                 }
                 newWords.some(w => w.word === newWord) ? null : newWords.push(newWord);
             });
             setAllWords((prev) => [...prev, ...newWords.filter(word => !prev.some(w => w.word === word.word))]);
-        }else{
+        } else {
             console.error("not getting words");
         }
-        
+
     }
 
     //adding new user into the database for his/her words storage
     const addUserIntoDatabase = async (name, email, language, avatarIndex) => {
         let errorAddingUser = '';
         try {
-            await addDoc(collection(wordsDB, "users"),{
-                name : name,
-                email : email, 
-                language : language,
-                avatarIndex : avatarIndex,
+            await addDoc(collection(wordsDB, "users"), {
+                name: name,
+                email: email,
+                language: language,
+                avatarIndex: avatarIndex,
             });
-            
+
         } catch (error) {
             errorAddingUser = error;
             throw error;
         }
-        if(errorAddingUser){
+        if (errorAddingUser) {
             console.error(errorAddingUser);
-        }else{
+        } else {
             return true;
         }
     }
 
 
     //reset password
-    const resetUserPassword = async() => {
-        sendPasswordResetEmail(auth , email)
-        .then(()=>{
-            console.log("password email sent");
-        })
-        .catch((error)=>{
-            console.error("error in sending reset password msg", error);
-        })
+    const resetUserPassword = async () => {
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                console.log("password email sent");
+            })
+            .catch((error) => {
+                console.error("error in sending reset password msg", error);
+            })
     }
 
-   
+
 
     return (
         <useFirebase.Provider value={{ createUser, signInUser, name, setName, email, setEmail, password, setPassword, isLoggedIn, logOut, user, getAllWords, allWords, setAllWords, addUserIntoDatabase, userID, setUserID, resetUserPassword, avatarIndex, setAvatarIndex }}>
